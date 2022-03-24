bdbb430a4c88c9a0f40f90d3cdec01eec9d5d0df
A1suluu <baisheva2004@gmail.com>
2022-03-24 17:40:31 +0600

    delete let c for

--- lesson66/app.js
+++ lesson66/app.js
@@ -1,7 +1,7 @@
-for (let a = 1; a <= 1000; a++) {
-    for (let b = 1; b <= 1000; b++) {
-        for (let c = 1; c <= 1000; c++) {
-            if ((a ** 2 + b ** 2 === c ** 2) && (a < b && b < c) && (a + b + c === 1000)) {
+for (let a = 1; a <= 200; a++) {
+    for (let b = a; b <= 400; b++) {
+        for (let c = b; c <= 500; c++) {
+            if ((a ** 2 + b ** 2 === c ** 2) && (a + b + c === 1000)) {
                 console.log(a, b, c)
             }
         }
