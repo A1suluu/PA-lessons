711b5a2e5505c31b24355e43794298a6f066c6d6
A1suluu <baisheva2004@gmail.com>
2022-03-24 17:48:24 +0600

    the max fast option of code

--- lesson66/app.js
+++ lesson66/app.js
@@ -1,9 +1,9 @@
-for (let a = 1; a <= 200; a++) {
-    for (let b = a; b <= 400; b++) {
-        for (let c = b; c <= 500; c++) {
-            if ((a ** 2 + b ** 2 === c ** 2) && (a + b + c === 1000)) {
-                console.log(a, b, c)
-            }
+for (let a = 1; a <= 500; a++) {
+    for (let b = a; b <= 500; b++) {
+        let c = 1000 - a - b
+        if (a ** 2 + b ** 2 === c ** 2) {
+            console.log(a, b, c)
+            break
         }
     }
 }
