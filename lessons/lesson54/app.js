87172447552e66286cc0a4f741466b6d0fd6c83c
A1suluu <baisheva2004@gmail.com>
2022-04-26 08:49:09 +0600

    add changes of 26,54, 38,67 files

--- lessons/lesson54/app.js
+++ lessons/lesson54/app.js
@@ -1,53 +1,28 @@
 var app_id = "d844456b080641219066aa386c5144f5"
 
-document.addEventListener("DOMContentLoaded", () => setInterval (main, 3000))
+document.addEventListener(
+    "DOMContentLoaded",
+    () => setInterval(main, 3000)
+)
 
 function main () {
     let url = `https://openexchangerates.org/api/latest.json?app_id=${app_id}`
     fetch(url).then(response => response.json()).then(data => {
-console.log(data)
+        console.log(data)
+        
+        let usd = data.rates.KGS
+        const element = document.getElementById("usdValue")
+        
+        let eur = data.rates.EUR
+        eur = Number(usd) / Number(eur)
+        const element2 = document.getElementById("eurValue")
+        
+        let rub = data.rates.RUB
+        rub = Number(usd) / Number(rub)
+        const element3 = document.getElementById("rubValue")
 
-let usd = data.rates.KGS
-const elementUSD = document.getElementById("usdValue")
-
-let eur = data.rates.EUR
-eur = Number(usd) / Number(eur)
-const elementEUR = document.getElementById("eurValue")
-
-let rub = data.rates.RUB
-rub = Number(usd) / Number(rub)
-const elementRUB = document.getElementById("rubValue")
-
-elementUSD.innerText = Math.round(usd * 100) / 100;
-elementEUR.innerText = Math.round(eur * 100) / 100;
-    elementRUB.innerText = Math.round(rub * 100) / 100;
-
-
-// const search_input = document.getElementsByClassName("search_input")
-// const userValue = document.getElementById("userCurValue")
-
-// let userTxt = document.getElementById("userCur")
-// let result = data.rates.userTxt
-// result = Number(usd) / Number(result)
-// userValue.innerText = Math.round(result * 100) / 100;
-
-
-
-})
-
+        element.innerText = Math.round(usd * 100) / 100
+        element2.innerText = Math.round(eur * 100) / 100
+        element3.innerText = Math.round(rub * 100) / 100
+    })
 }
-
-
-
-// function submitForm() {
-    //     let url = `https://openexchangerates.org/api/latest.json?app_id=${app_id}`
-    
-//     fetch(url).then(response => response.json()).then(data => {
-//         console.log(data)
-//     })
-//     .then(data => {
-
-//         let txt = search_input.innerText 
-//         txt 
-//     })
-// }
